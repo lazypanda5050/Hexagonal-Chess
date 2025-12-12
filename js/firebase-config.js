62a8da29b9f0edef1d6e1348ca161738bf660f56
lazypanda5050 <w.n.lazypanda5050@gmail.com>
2025-12-11 16:33:38 -0800

    trying to fix git merge stuff

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Populate this object with your Firebase project's public configuration.
+// The login widget will stay disabled until these placeholder values are replaced.
+globalThis.FIREBASE_CONFIG = globalThis.FIREBASE_CONFIG ?? {
+    apiKey: "AIzaSyDyHzOkUqaG8umPKxobR5ga0G6Gr8TpMW4",
+    authDomain: "hexagonal-chess.firebaseapp.com",
+    databaseURL: "https://hexagonal-chess-default-rtdb.firebaseio.com",
+    projectId: "hexagonal-chess",
+    storageBucket: "hexagonal-chess.firebasestorage.app",
+    messagingSenderId: "629805933278",
+    appId: "1:629805933278:web:49c666f8a87ce6751b184f",
+    measurementId: "G-DKBT4BMSXD"
+};
